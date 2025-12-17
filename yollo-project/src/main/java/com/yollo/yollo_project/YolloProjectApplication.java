2d562e05813d4db5c7afa862eb185343d6f40b43
Houcine <lahoucinegahboub@gmail.com>
2025-12-17 15:42:15 +0100

    spring initialisation

---
+++ yollo-project/src/main/java/com/yollo/yollo_project/YolloProjectApplication.java
@@ -0,0 +1,13 @@
+package com.yollo.yollo_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YolloProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YolloProjectApplication.class, args);
+	}
+
+}
---
+++ yollo-project/src/test/java/com/yollo/yollo_project/YolloProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yollo.yollo_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YolloProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
